b9f783ceee7bd723b104dce670054331c5169c12
sarfrajm <sarfraj.mulani@enyotalearning.com>
2019-02-06 18:11:41 +0530

    First commit

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+phpinfo();
+/*
+//START:24 hrs To AP/PM Conversion.
+
+$date = '2017-11-20 05:55:42'; 
+echo date('h:i:s a Y-d-y', strtotime($date));
+
+//END:24 hrs To AP/PM Conversion.
+*/
+?>
